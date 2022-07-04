e994ad98745ca2e52f36505803162b224067565c
Punithmsit <punith.msit@gmail.com>
2022-07-04 23:15:57 +0530

    Commit on main

--- service/server.js
+++ service/server.js
@@ -9,6 +9,10 @@ app.get('/greeting/:communityID', (req, res) => {
   res.send(`Hey ${req.params.communityID}, really nice seeing you here! 🙂`);
 });
 
+app.get('/welcome/:welcomeID', (req, res) => {
+  res.send(`Hi ${req.params.welcomeID}, Welcome to Community Code Challenge!!!`)
+});
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}...`);
